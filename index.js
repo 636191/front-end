4c11b3f4928564c9a602654522ec63358b95cbb9
636191 <122289124+636191@users.noreply.github.com>
2023-03-25 16:06:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("iam satish")
+
+let apples = '10';
+apples = '20'
+bannaa = 30
+console.log( 'the no of apples',apples)
+console.log( 'the no of apples',apples)
+console.log( 'the no of apples',bannaa)
+
+let myname="satish"
+console.log(myname)
+
+
+
+let gargipassed=true
+let gargiFailed=false
+console.log(typeof(gargipassed))
+console.log(typeof(gargiFailed))

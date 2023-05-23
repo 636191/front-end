5ffb138930fedbe3dc55c8ab7a0dc00c1a0dd0ab
Satish <122289124+636191@users.noreply.github.com>
2023-05-23 22:43:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-console.log("iam satish")
+/*events */
+//const btn = document.querySelector('#btn')
+/* function multiply(a,b){
+  return a*b
+}
+multiply(23,45) */
+/*btn.addEventListener("click",function amiclicked(){
+    alert("iam clicled now")
+})*/ 
 
-let apples = '10';
-apples = '20'
-bannaa = 30
-console.log( 'the no of apples',apples)
-console.log( 'the no of apples',apples)
-console.log( 'the no of apples',bannaa)
+const btn1 = document.querySelecto=r('#btn1')
+btn1.addEventListener('click',toggle)
 
-let myname="satish"
-console.log(myname)
+function toggle(){
+    let title=document.querySelector("#title")
+
+    if(title.style.display !=="none"){
+        title.style.display = "none"
+    }else{
+        title.style.display="block"
+    }
+}
+const itemlist=document.querySelectorAll('li')
+console.log( itemlist)
+ 
+for(let i of itemlist){
+    console.log(i)
+    i.innerText="yaaaaaay"
+}
+const para=document.querySelector(#para)
+let count = 0
+para.addEventListener("click",function paraclick(event){
+           console.log(event)
+           count++
+           console.log(count)
+})
 
 
-
-let gargipassed=true
-let gargiFailed=false
-console.log(typeof(gargipassed))
-console.log(typeof(gargiFailed))
